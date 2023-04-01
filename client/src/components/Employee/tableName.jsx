a787c75cd00a366f915b60d4a6d24bc654e59172
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-04-01 11:47:48 +0700

    1212312121

--- client/src/components/Employee/tableName.jsx
+++ client/src/components/Employee/tableName.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+<<<<<<< HEAD
 // import EditButton from './editButton';
 // import EditForm from './editFrom';
 // import ViewButton from './ViewButton';
@@ -11,11 +12,20 @@ import { Column } from 'primereact/column';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+=======
+import EditButton from './editButton';
+import EditForm from './editFrom';
+import ViewButton from './ViewButton';
+import ViewFrom from './ViewFrom';
+>>>>>>> parent of 39c15cfb (1/4/66)
 
 const TableName = () => {
     // Array
     const [data, setData] = useState([]);
+<<<<<<< HEAD
     const [count, setCount] = useState('');
+=======
+>>>>>>> parent of 39c15cfb (1/4/66)
 
     // ********************* start button **************************** //
     const [showModal, setShowModal] = useState(false);
@@ -23,6 +33,12 @@ const TableName = () => {
     const [selectedEmployee, setSelectedEmployee] = useState(null);
     // ******************** end button ***************************** //
 
+<<<<<<< HEAD
+=======
+
+    const [count, setCount] = useState('');
+
+>>>>>>> parent of 39c15cfb (1/4/66)
 
     // table
     useEffect(() => {
@@ -53,6 +69,7 @@ const TableName = () => {
                 })
             )
         })
+<<<<<<< HEAD
         // alert('ลบข้อมูลพนักงานเรียบร้อยแล้ว');
     }
 
@@ -106,10 +123,34 @@ const TableName = () => {
                 return null;
         }
     };
+=======
+        alert('ลบข้อมูลพนักงานเรียบร้อยแล้ว');
+    }
+
+
+    const handleEditClick = (employee) => {
+        setSelectedEmployee(employee);
+        setShowModal(true);
+    }
+
+    const handleViewClick = (employee) => {
+        setSelectedEmployee(employee);
+        setViewModal(true);
+    }
+
+    const handleModalClose = () => {
+        setShowModal(false);
+    }
+
+    const handleViewModalClose = () => {
+        setViewModal(false);
+    }
+>>>>>>> parent of 39c15cfb (1/4/66)
 
     return (
         <div className="">
             <p className="mt-5 text-indigo-700 font-medium">ข้อมูลพนักงานจำนวน {count}  รายการ</p>
+<<<<<<< HEAD
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-5">
                 <DataTable value={data}
                     showGridlines
@@ -129,6 +170,59 @@ const TableName = () => {
             </div>
 
             
+=======
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
+                <table className="w-full text-gray-500 dark:text-gray-400">
+                    <thead className="lg:text-sm sm1:text-xs text-gray-700 uppercase bg-indigo-200 text-center">
+                        <tr>
+                            <th scope="col" className="px-2 py-">ลำดับ</th>
+                            <th scope="col" className="px-2 py-3">รหัสพนักงาน</th>
+                            <th scope="col" className="px-7 py-3">ชื่อจริง</th>
+                            <th scope="col" className="px-6 py-3">นามสกุล</th>
+                            <th scope="col" className="px-6 py-3">ชื่อเล่น</th>
+                            <th scope="col" className="px-6 py-3">สาขา</th>
+                            <th scope="col" className="px-6 py-3">แผนก</th>
+                            <th scope="col" className="px-6 py-3">สถานะ</th>
+                            <th scope="col" className="px-6 py-3">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={item.id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-xs">
+                                <td className="border px-4 py-2 text-center">{index + 1}</td>
+                                <td className="border px-4 py-2 text-center">{item.Emp_Code}</td>
+                                <td className="border px-4 py-2">{item.NameTH}</td>
+                                <td className="border px-4 py-2">{item.SurnameTH}</td>
+                                <td className="border px-4 py-2">{item.Nickname}</td>
+                                <td className="border px-4 py-2">{item.Branch}</td>
+                                <td className="border px-4 py-2">{item.Department}</td>
+                                <td className="border px-4 py-2">{item.Status}</td>
+                                <td className="border px-2 py-2 space-x-2 text-center md:justify-center">
+                                    <ViewButton onClick={() => handleViewClick(item)} />
+                                    <EditButton onClick={() => handleEditClick(item)} />
+                                    <button className='bg-red-200 hover:text-red-200 border-red-200' onClick={() => { deleteData(item.id) }}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#d0021b" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    {
+                        showModal && (
+                            <EditForm data={selectedEmployee} onClose={handleModalClose} />
+                        )
+                    }
+
+
+                    {
+                        ViewModal && (
+                            <ViewFrom data={selectedEmployee} onClose={handleViewModalClose} />
+                        )
+                    }
+                    {/* ******************* end modals ********************** */}
+                </table>
+            </div>
+>>>>>>> parent of 39c15cfb (1/4/66)
         </div>
     );
 };
